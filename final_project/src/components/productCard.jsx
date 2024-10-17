b1d024223050137d3274d4554ca9bfc1786d08fc
Montagné Ihsane <146075507+MontagneIhsane@users.noreply.github.com>
2024-10-17 14:09:28 +0200

    Update productCard.jsx

--- final_project/src/components/productCard.jsx
+++ final_project/src/components/productCard.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import './productCard.css';
 
 const ProductCard = ({ product, addToCart }) => {
     return (
         <div className="product-card">
-            <img src={product.image} alt={product.name} className="product-image" />
-            <h2 className="product-name">{product.name}</h2>
-            <p className="product-price">{product.price}</p>
-            <button className="add-to-cart-button" onClick={() => addToCart(product)}>
-                Add to Cart
-            </button>
+            <img src={product.image} alt={product.name} />
+            <h2>{product.name}</h2>
+            <p>{product.price}</p>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
         </div>
     );
 };
