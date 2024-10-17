8a1e71f17ae57c7c0f424a9f08b4cabab70c6676
Montagné Ihsane <146075507+MontagneIhsane@users.noreply.github.com>
2024-10-17 14:10:21 +0200

    Update productGallery.jsx

--- final_project/src/components/productGallery.jsx
+++ final_project/src/components/productGallery.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
 import ProductCard from './productCard';
-import './productGallery.css'
+import './productGallery.css';
 
-const ProductGallery = () => {
-    const products = [
-        { id: 1, name: 'One Million', price: '$75', image: 'one-million.jpg' },
-        { id: 2, name: 'Bleu de Chanel', price: '$70', image: 'bleu-de-chanel.jpg' },
-        { id: 3, name: 'Irresistible', price: '$100', image: 'irresistible.jpg' },
-        { id: 4, name: 'Jean Paul Gaultier', price: '$90', image: 'JP-Gaultier.webp' },
-    ];
-
-    const addToCart = (product) => {
-        console.log(`${product.name} has been added to the cart.`);
-    };
-
+const ProductGallery = ({ products, addToCart }) => {
     return (
         <div className="product-gallery">
-            {products.map((prod) => (
-                <ProductCard key={prod.id} product={prod} addToCart={addToCart} />
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} addToCart={addToCart} />
             ))}
         </div>
     );
 };
 
 export default ProductGallery;
+
+
+
 
